fbea88753fee4a2e4bb4a34d9a55963815931599
Sergiu Bucur <sergiu.bucur@outlook.com>
2018-11-17 19:34:02 +0200

    use Consolas font if available; add "Load sample file"

--- src/components/info-display/InfoDisplay.js
+++ src/components/info-display/InfoDisplay.js
@@ -8,13 +8,19 @@ export default class InfoDisplay extends Component {
 		EventBus.events.dispatch("code-file-picker-update", { open: true });
 	}
 
+	handleSampleClick = () => {
+		window.sessionStorage.removeItem("ftcLoadFromUrl");
+		window.location.reload();
+	}
+
 	render() {
 		return (
 			<div className="info-display">
 				<i className="fa fa-arrow-circle-left" /><i className="fa fa-arrow-circle-right" /> Move<br />
 				<i className="fa fa-arrow-circle-up" /> Jump<br /><br />
 
-				<span className="action" onClick={this.handleOpenClick}>Load code file</span><br /><br />
+				<span className="action" onClick={this.handleOpenClick}>Open code file</span><br />
+				<span className="action" onClick={this.handleSampleClick}>Load sample file</span><br /><br />
 
 				<span className="credits">
 					-------------<br/>
